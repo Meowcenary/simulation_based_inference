52e1101ebc45bdf861bf0548f6bbdcf7c2a6ffa5
Eric Newman <newmaneric5@gmail.com>
2023-12-03 12:52:54 -0600

    Generate Vector / Generate Matrix Functions
    - Add functions to generate vectors and matricies for testing

--- util/util.go
+++ util/util.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+  "math/rand"
   "reflect"
   "runtime"
   "strings"
@@ -13,9 +14,58 @@ as the argument "statistic" to the two Boot functions (BootVector, BootMatrix) d
 params:
 function - an interace is used to genericize the function, but the value should be a pointer to a function
 
-returns: the name of the function as a string
+returns:
+the name of the function as a string
 */
 func GetFunctionName(function interface{}) string {
   strs := strings.Split((runtime.FuncForPC(reflect.ValueOf(function).Pointer()).Name()), ".")
   return strs[len(strs)-1]
+}
+
+/*
+public function for generating a random vector (Golang slice) of float64 values for use by boot
+
+params:
+r - pointer to an instance of rand
+size - integer size of the vector to be generated
+
+returns:
+slice of float64 values that is size values long
+*/
+func GenerateRandomVector(r *rand.Rand, size int) []float64 {
+  vector := make([]float64, size)
+
+  for i := 0; i < size; i++ {
+    vector[i] = rand.NormFloat64()
+  }
+
+  return vector
+}
+
+/*
+public function for generating a rows by by columns matrix (Golang 2d slice) of float64 values for use by boot
+
+params:
+r - pointer to an instance of rand
+rows - number of rows for the matrix to be generated
+columns - number of columns for the matrix to be generated
+
+returns:
+matrix (2d slice) of float64 values that has the specified number of rows and columns
+*/
+func GenerateRandomMatrix(r *rand.Rand, rows, columns int) [][]float64 {
+  // create an empty matrix to hold values
+  matrix := make([][]float64, rows)
+  for i := 0; i < rows; i++ {
+    matrix[i] = make([]float64, columns)
+  }
+
+  // populate matrix
+  for i := 0; i < rows; i++ {
+    for j := 0; j < columns; j++ {
+      matrix[i][j] = rand.NormFloat64()
+    }
+  }
+
+  return matrix
 }
--- util/util_test.go
+++ util/util_test.go
@@ -1,9 +1,20 @@
 package util
 
 import (
+	"math/rand"
 	"testing"
 )
 
+// rand instance for tests
+func seed() int64 {
+	return 123
+}
+
+func random() *rand.Rand {
+  return rand.New(rand.NewSource(seed()))
+}
+
+// basic functions for testing GetFunctionName
 func NamedFunction(param int) int { return param }
 func AnotherNamedFunction(param float64) float64 { return param }
 
@@ -18,5 +29,29 @@ func TestGetFunctionName(t *testing.T) {
 	name = GetFunctionName(AnotherNamedFunction)
 	if expectedName != name {
 		t.Errorf("Expected: %s\nGot: %s", expectedName, name)
+	}
+}
+
+func TestGenerateRandomVector(t *testing.T) {
+	expectedVectorLength := 10
+	generatedVector := GenerateRandomVector(random(), 10)
+	generatedVectorLength := len(generatedVector)
+
+	if expectedVectorLength != generatedVectorLength {
+		t.Errorf("Expected vector of length: %d, Got vector of length: %d", expectedVectorLength, generatedVectorLength)
+	}
+}
+
+func TestGenerateRandomMatrix(t *testing.T) {
+	expectedRows := 3
+	expectedColumns := 4
+	generatedMatrix := GenerateRandomMatrix(random(), expectedRows, expectedColumns)
+	generatedRows := len(generatedMatrix)
+	generatedColumns := len(generatedMatrix[0])
+
+	if expectedRows != generatedRows {
+		t.Errorf("Expected %v rows\nGot %v rows", expectedRows, generatedRows)
+	} else if expectedColumns != generatedColumns {
+		t.Errorf("Expected %v rows\nGot %v rows", expectedColumns, generatedColumns)
 	}
 }
